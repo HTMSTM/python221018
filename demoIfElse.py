c7d9736456336e5009e4553ddf4110abfb05ad3e
HTMSTM <tinyisle01@gmail.com>
2022-10-18 13:57:37 +0900

    분기반복문

---
+++ demoIfElse.py
@@ -0,0 +1,24 @@
+# demoIfElse.py
+# 블럭으로 주석처리 = ctrl + /
+score = int(input("점수를 입력 :"))
+if 90 <= score <= 100:
+    grade = "A"
+elif 80 <= score < 90:
+    grade = "B"
+elif 70 <= score < 80:
+    grade = "C"
+else:
+    grade = "D"
+
+print("등급은 ", grade)
+
+# 딕셔너리
+d = {"apple":100, "kiwi":200}
+for k in d.keys():
+    print(k)
+
+for v in d.values():
+    print(v)
+
+for k, v in d.items():
+    print(k, v)
