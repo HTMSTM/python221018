7ffa1071a13b6535f57717c45a8d9bee7948b649
HTMSTM <tinyisle01@gmail.com>
2022-10-18 15:46:41 +0900

    class 5

--- demoIfElse.py
+++ demoIfElse.py
@@ -1,24 +1,34 @@
 # demoIfElse.py
 # 블럭으로 주석처리 = ctrl + /
-score = int(input("점수를 입력 :"))
-if 90 <= score <= 100:
-    grade = "A"
-elif 80 <= score < 90:
-    grade = "B"
-elif 70 <= score < 80:
-    grade = "C"
-else:
-    grade = "D"
+# score = int(input("점수를 입력 :"))
+# if 90 <= score <= 100:
+#     grade = "A"
+# elif 80 <= score < 90:
+#     grade = "B"
+# elif 70 <= score < 80:
+#     grade = "C"
+# else:
+#     grade = "D"
 
-print("등급은 ", grade)
+# print("등급은 ", grade)
 
-# 딕셔너리
-d = {"apple":100, "kiwi":200}
-for k in d.keys():
-    print(k)
+# # 딕셔너리
+# d = {"apple":100, "kiwi":200}
+# for k in d.keys():
+#     print(k)
 
-for v in d.values():
-    print(v)
+# for v in d.values():
+#     print(v)
 
-for k, v in d.items():
-    print(k, v)
+# for k, v in d.items():
+#     print(k, v)
+
+print("--= break ---")
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+
+for i in lst:
+    if i > 5:
+        break
+    print("item:{0}".format(i))
+    print("item: ", i)
