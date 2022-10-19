5a7fe838d13704951f41f2bd34c4dd91371d3e48
HTMSTM <tinyisle01@gmail.com>
2022-10-19 16:58:21 +0900

    에러처리

--- try1.py
+++ try1.py
@@ -3,10 +3,17 @@
 #함수 정의
 def divide(a, b):
     return a/b
-
-# 함수 호출
-result = divide(5, 0)
-print("결과 : {0}".format((result)))
+try:
+    # 함수 호출
+    result = divide(5, 2)
+except TypeError:
+    print("숫자여야 합니다")
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다.")
+else:
+    print("결과 : {0}".format((result)))
+finally:
+    print("무조건 실행(한번 더 체크)")
 
 
 print("---전체 코드 실행 종료")
