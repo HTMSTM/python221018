efaa39deefbb7c5473c7df6d6d2038ab644cab71
HTMSTM <tinyisle01@gmail.com>
2022-10-20 16:56:59 +0900

    클리앙 중고장터

--- web3.py
+++ web3.py
@@ -5,6 +5,9 @@ import urllib.request
 #웹크롤링
 from bs4 import BeautifulSoup
 
+#파일로 저장
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
+
 #페이징 처리
 for i in range(1,6):
     url = "http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri&page=" + str(i)
@@ -21,12 +24,15 @@ for i in range(1,6):
     title = cartoons[0].find("a").text
     link = cartoons[0].find("a")["href"]
 
-    print(title)
-    print(link)
+    #print(title)
+    #print(link)
 
     #<td class = "title">
-
-
+    #   <a href="/webtoon/detail?">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+    # </td>
     for item in cartoons:
         title = item.find("a").text
         print(title)
+        f.write(title + "\n")
+
+f.close()
